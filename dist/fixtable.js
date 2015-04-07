ffc73e7682662bd43e5dd819561fd33446d45f2e
Trezy <trezy.peebles@inin.com>
2015-04-07 16:22:05 -0500

    Add UMD support

--- dist/fixtable.js
+++ dist/fixtable.js
@@ -1,3 +1,19 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define([], function () {
+      return (factory());
+    });
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    factory();
+  }
+}(this, function () {
+
 var Fixtable;
 
 Fixtable = (function() {
@@ -168,3 +184,6 @@ Fixtable = (function() {
 })();
 
 //# sourceMappingURL=fixtable.js.map
+
+
+}));
---
+++ grunt/umd.coffee
@@ -0,0 +1,5 @@
+module.exports = ( grunt, options ) ->
+  fixtable:
+    options:
+      src: 'dist/fixtable.js'
+      dest: 'dist/fixtable.js'
